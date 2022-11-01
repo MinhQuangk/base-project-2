9a0f10d26826e41ea538a942fbe3f37ad26ee4cd
Quang <quangvm.21it@vku.udn.vn>
2022-11-01 09:54:01 +0700

    day3

--- resources/views/layout/assets/dashboard.blade.php
+++ resources/views/layout/assets/dashboard.blade.php
@@ -1,35 +1,215 @@
 <section class="dashboard">
-    <div class="welcome_admin" style="display: flex;align-items: center;justify-content:space-between ">
-        <h1 style="padding: 1% 4%;font-size: 34px;color: #000;width: 20%;">Dashboard</h1>
-        <div class="welcome_admin_message" style="width:80%;display: flex;align-self: center;background-color: darkgrey;color: #000;padding: 20px;margin: 15px 0px">
-            <h1 style="margin: 0px 15px">
-                @php
-                    $time = date("H");
-                    if($time<"12"){
-                        echo "Good Morning";
-                    }elseif ($time>"12" && $time<"17") {
-                        echo "Good Afternoon";
-                    }elseif($time >"17" && $time < "18") {
-                        echo "Good Evening";
-                    }else {
-                        echo "Good Night" ;
-                    }
-
-                @endphp
+    <div class="welcome_admin" style="display: flex; align-items: center;justify-content: space-between;">
+        <h1 style="padding: 1% 4%; font-size:34px; color:#000; width:20%">Dashboard</h1>
+        <div class="welcome_admin_message" style="width: 80%;display:flex; align-items: center;background-color: yellowgreen;color: #fff;padding: 20px;margin: 15px 0px">
+            <h1 style="margin: 0px 15px;">
+                <?php
+                $time = date("H");
+                if($time < "12"){
+                    echo"Good Morning";
+                }else if($time >="12" && $time < "17"){
+                    echo "Good Afternoon";
+                }else if($time >="17" && $time < "19"){
+                    echo "Good Evening";
+                }else if($time >= "19"){
+                    echo "Good Night";
+                }
+                ?>
             </h1>
-            <h1 style="color:yellow;font-size: 35px"> Quang</h1>
-            <h1 style="font-size: 35px;margin-left: 200px"
-            
-            >@php
-            echo date("F j, Y, g:i a")
-        @endphp</h1>
+            <h1 style="color: yellow;font-size: 35px">Văn Thiên</h1>
+            <h1 style="font-size: 35px;margin-left: 200px;"><?php echo date('M d,Y'); ?></h1>
         </div>
     </div>
-    <div class="cover-img">
-        <img src="{{asset('font/img/cartoon-urban-cityscape-with-college-academy-students-university-architecture-background_212168-968.jpg')}}" alt="cover-img">
-        <span class="text">
-            <h1>Student management system</h1>
-            <p>Hệ thống quản lý và đào tạo sinh viên</p>
-        </span>
+    <div class="cover-img"> 
+    <img src="{{ asset('font/img/schooling.jpg') }}" alt="cover image">
+    <span class="text">
+        <h1>School System</h1>
+        <p>Education is our passport to the futute, for tomorow belongs to the<br>
+            people who prepare for it today</p>
+    </span>
+    </div>
+</section>
+<section class="all-info">
+    <div class="row">
+        <div class="col">
+            <div id="teacher" class="card">
+                <div class="teacher-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="teacherCount">01</h1>
+                            <p>Giảng viên</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="far fa-chart-bar"></i>
+                    </div>
+                </div>
+                <div class="teacher-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+            <div id="attendance" class="card">
+                <div class="attendance-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="examCount">01</h1>
+                            <p>Điểm thi</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-user-plus"></i>
+                    </div>
+                </div>
+                <div class="teacher-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div id="student" class="card">
+                <div class="student-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="studentCount">01</h1>
+                            <p>Sinh viên</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-user"></i>
+                    </div>
+                </div>
+                <div class="student-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+            <div id="media" class="card">
+                <div class="media-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="mediaCount">01</h1>
+                            <p>Danh bạ</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-user-plus"></i>
+                    </div>
+                </div>
+                <div class="media-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div id="subjects" class="card">
+                <div class="subjects-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="subjectsCount">01</h1>
+                            <p>Môn học</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-plus-circle"></i>
+                    </div>
+                </div>
+                <div class="subjects-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+            <div id="scores" class="card">
+                <div class="scores-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="scoresCount">01</h1>
+                            <p>Điểm học phần</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-print"></i>
+                    </div>
+                </div>
+                <div class="scores-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div id="schedule" class="card">
+                <div class="schedule-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="scheduleCount">01</h1>
+                            <p>Lịch học</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-clock"></i>
+                    </div>
+                </div>
+                <div class="schedule-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+            <div id="expenses" class="card">
+                <div class="expenses-inner">
+                    <div class="column column-left">
+                        <div class="column-left-inner">
+                            <h1 id="expensesCount">01</h1>
+                            <p>Học phí</p>
+                        </div>
+                    </div>
+                    <div class="column column-right">
+                        <i class="fas fa-money-check-alt"></i>
+                    </div>
+                </div>
+                <div class="expenses-footer">
+                    <a href="#">More info <i class="fas fa-angle-double-alt"></i></a>
+                </div>
+            </div>
+        </div>  
+    </div>
+</section>
+<section class="chart">
+    <div class="row">
+        <div class="chart_left">
+            <h1>All Teacher</h1>
+            <canvas id="mychart2" width="400" height="400"></canvas>
+            <div class="chart_footer">
+              <div class="row">
+                <div class="chart_footer_left">
+                    <hr>
+                    <span>Female Teacher</span>
+                    <h1 id="fetchFemale_teacher_count">00</h1>
+                </div>
+                <div class="chart_footer_right">
+                    <hr>
+                    <span>Male Teacher</span>
+                    <h1 id="fetchMale_teacher_count">00</h1>
+                </div>
+              </div>  
+            </div>
+        </div>
+        <div class="chart_right">
+            <div class="top_male_female_count">
+                <h1>All Student</h1>
+                <button onclick="window.location.reload()">Refresh</button>
+            </div>
+            <canvas id="myChart3" width="400" height="400"></canvas>
+            <div class="chart_footer">
+                <div class="row">
+                    <div class="chart_footer_left">
+                        <hr>
+                        <h1 id="fetchChartData"></h1>
+                        <span>Female Students</span>
+                        <h1 id="fetchFemale_count">00</h1>
+                    </div>
+                    <div class="chart_footer_right">
+                        <hr>
+                        <span>Male Students</span>
+                        <h1 id="fetchMale_count">00</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,14 +1,131 @@
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Student management system</title>
-    <script src="https://kit.fontawesome.com/285f021aec.js" crossorigin="anonymous"></script>
-    
-    <link rel="stylesheet" href="{{asset("font/css/dashboard.css")}}">
+    <title>School Management System</title>
+    <link href="{{ asset('font/css/admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('font/css/dashboard.css') }}" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
+
 <body>
-    @include('layout.assets.dashboard')
+    <section class="admin-top">
+        <div class=row>
+            <div class="admin-left" id="slideNav">
+                <div class="admin">
+                    <img src="{{ asset('font/img/yasua.jpg') }}" alt="profile picture" />
+                    <h4 style="text-align:center;">T.D.V.Thiên</h4>
+                </div>
+                <div class="tab">
+                    <div class="tablinks" id="defaultOpen" onclick="openTab(event,'dashboard_top')">
+                        <i class="fas fa-tachometer-alt"></i>
+                        <span class="tooltip">Trang chủ</span>
+                        <h4>Trang chủ</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'teacher_top')">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                        <span class="tooltip">Giảng viên</span>
+                        <h4>Giảng viên</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'student')">
+                        <i class="fas fa-user"></i>
+                        <span class="tooltip">Sinh viên</span>
+                        <h4>Sinh viên</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'subjects')">
+                        <i class="fas fa-plus-circle"></i>
+                        <span class="tooltip">Môn học</span>
+                        <h4>Môn học</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'scores')">
+                        <i class="fas fa-print"></i>
+                        <span class="tooltip">Điểm số</span>
+                        <h4>Điểm số</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'schedule')">
+                        <i class="fas fa-clock"></i>
+                        <span class="tooltip">Thời khóa biểu</span>
+                        <h4>Thời khóa biểu</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'media')">
+                        <i class="fas fa-video"></i>
+                        <span class="tooltip">Danh bạ</span>
+                        <h4>Danh bạ điện thoại</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'expenses')">
+                        <i class="fas fa-money-check-alt"></i>
+                        <span class="tooltip">Học phí</span>
+                        <h4>Học phí</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'exams')">
+                        <i class="fas fa-graduation-cap"></i>
+                        <span class="tooltip">Exam</span>
+                        <h4>Exam</h4>
+                    </div>
+                    <div class="tablinks" onclick="openTab(event,'map')">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <span class="tooltip">Vị trí</span>
+                        <h4>Vị trí</h4>
+                    </div>
+                </div>
+            </div>
+
+            <!-- right side of dashboard start here ======== -->
+            <div class="admin-right" id="admin-right">
+                <div class="header">
+                    <div class="header-left">
+                        <i onclick="menuAnimation()" class="fas fa-bars"></i>
+                    </div>
+                    <div class="header-right">haha</div>
+                </div>
+                <div id="dashboard_top" class="tabcontent">
+                    @include('layout.assets.dashboard');
+                </div>
+                <div id="teacher_top" class="tabcontent">
+                    <h2>teacher content</h2>
+                </div>
+                <div id="student" class="tabcontent">
+                    <h2>Student content</h2>
+                </div>
+                <div id="subjects" class="tabcontent">
+                    <h2>Subjects content</h2>
+                </div>
+                <div id="scores" class="tabcontent">
+                    <h2>Scores content</h2>
+                </div>
+                <div id="schedule" class="tabcontent">
+                    <h2>Schedule content</h2>
+                </div>
+                <div id="media" class="tabcontent">
+                    <h2>Media content</h2>
+                </div>
+                <div id="expenses" class="tabcontent">
+                    <h2>expenses content</h2>
+                </div>
+                <div id="exams" class="tabcontent">
+                    <h2>Exams content</h2>
+                </div>
+                <div id="map" class="tabcontent">
+                    <h2>Map content</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+<script>
+    function menuAnimation(){
+        var element = document.getElementById("slideNav");
+        var element1 = document.getElementById("admin-right");
+        if(element && element1){
+            element.classList.toggle("navSlide");
+            element1.classList.toggle("navSlide1");
+        }
+    }
+</script>
 </body>
+
+
+<script src="{{ asset('font/js/app.js') }}"></script>
 </html>
